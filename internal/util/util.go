52b3d23c2ca475de28d5a44f884c518bc3374963
Ichinose Shogo <shogo82148@gmail.com>
2021-08-16 08:58:35 +0900

    escape backquotes in identifiers

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,7 +1,21 @@
 package util
 
+import "strings"
+
 // Backquote surrounds the given string in backquotes
 func Backquote(s string) string {
-	// XXX Does this require escaping
-	return "`" + s + "`"
+	var buf strings.Builder
+	// Strictly speaking, we need to count the number of backquotes in s.
+	// However, in many cases, s doesn't include backquotes.
+	buf.Grow(len(s) + len("``"))
+
+	buf.WriteByte('`')
+	for _, r := range s {
+		if r == '`' {
+			buf.WriteByte('`')
+		}
+		buf.WriteRune(r)
+	}
+	buf.WriteByte('`')
+	return buf.String()
 }
