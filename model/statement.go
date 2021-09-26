4274af7be1c6f45db725054db81f366b20d71928
Ichinose Shogo <shogo82148@gmail.com>
2021-09-26 23:06:22 +0900

    introduce Ident type

--- model/statement.go
+++ model/statement.go
@@ -1,5 +1,13 @@
 package model
 
+// Stmt is the interface to define a statement
+type Stmt interface {
+	ID() string
+}
+
+// Stmts describes a list of statements
+type Stmts []Stmt
+
 // Lookup looks for a statement with the given ID
 func (s Stmts) Lookup(id string) (Stmt, bool) {
 	for _, stmt := range s {
